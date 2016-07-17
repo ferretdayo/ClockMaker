938b2783810488a243af6b32e35110ce33e614fe
ferretdayo <ferretdayo@hotmail.co.jp>
2016-07-18 02:02:07 +0900

    cssいじった

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,25 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>ClockMaker</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <link href="{{asset('css/welcome.css')}}" rel="stylesheets" type="text/css" />
-
+        <link href="{{asset('/css/nomalize.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('/css/welcome.css')}}" rel="stylesheet" type="text/css" />
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+        <nav>
+            <ul>
+                <li class="title"><a href="#">ClockMaker</a></li>
+                <li class="menu"><a href="#">開発者</a></li>
+            </ul>
+        </nav>
+        <section>
+            <div class="container">
+                <div class="content">
+                    aaa
+                </div>
             </div>
-        </div>
+        </section>
     </body>
 </html>
