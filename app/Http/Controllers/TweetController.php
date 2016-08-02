a44dded4da5e5ac56b342cf4b2f71c67f07d81ef
ferretdayo <ferretdayo@hotmail.co.jp>
2016-08-03 07:22:11 +0900

    hoge

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -19,7 +19,7 @@ class TweetController extends BaseController
         // $access_token = env('ACCESS_TOKEN');
         // $access_token_secret = env('ACCESS_TOKEN_SECRET');
         $connection = new TwitterOAuth(env('API_KEY'), env('API_SECRET'));
-        $request_token = $connection->oauth('oauth/request_token');
+        $request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => function(){echo "aa";}));
         var_dump($request_token);
         $request->session()->put('oauth_token', $request_token['oauth_token']);
         $request->session()->put('oauth_token_secret', $request_token['oauth_token_secret']);
@@ -30,6 +30,8 @@ class TweetController extends BaseController
     }
 
     public function index(Request $request){
+        var_dump($request->input('oauth_token'));
+        var_dump($request->input('oauth_verifier'));
         if (!empty($request->input('oauth_token')) && !empty($request->input('oauth_verifier'))) {
             $connection = new TwitterOAuth(env('API_KEY'), env('API_SECRET'), $request->session()->get('oauth_token'), $request->session()->get('oauth_token_secret'));
             $access_token = $connection->oauth("oauth/access_token", ["oauth_verifier" => $request->input('oauth_verifier')]);
